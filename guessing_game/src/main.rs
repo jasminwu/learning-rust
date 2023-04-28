ef995fbf243aaf6d87b6e4a952bdd42192a725de
Jasmin Wu <jasminnwu@gmail.com>
2023-04-28 23:29:10 +1000

    Added ability to keep guessing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
-
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -9,13 +9,27 @@ fn main() {
 
     println!("The secret number is {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        // Convert guess from str to int
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
